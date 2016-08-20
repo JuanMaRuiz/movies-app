b16c2e2437552516148fb69b170336b6d5ba49d8
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-08-20 18:50:45 +0200

    Reorganized some tests, added new specs for movies lists, fixed some html code on profile status

--- tests/specs/login/loginSpec.js
+++ tests/specs/login/loginSpec.js
@@ -1,7 +1,7 @@
 describe('Bazinga Demo App', function() {
   it('should have a title', function() {
     browser.get('http://localhost:9000/');
-    expect(browser.getTitle()).toEqual('Bazinga Angular App');
+    expect(browser.getTitle()).toEqual('My movies Lists');
   });
 
   it('should be a login', function() {
