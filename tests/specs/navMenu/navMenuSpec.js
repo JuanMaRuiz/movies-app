4482df2e005fef7802b922ae9046884f7e29a9a4
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-08-21 10:41:29 +0200

    Using Page Objects to organize tests (applied to navbar only)

--- tests/specs/navMenu/navMenuSpec.js
+++ tests/specs/navMenu/navMenuSpec.js
@@ -1,16 +1,17 @@
 describe('Navigation menu', function() {
   var navBar;
-
+  
   beforeEach(function() {
-    navBar = element(by.id('navbar'));
-    browser.get('http://localhost:9000/');
+    navBar = require('./navBarPageObj.js');
+    navBarElem = new navBar()
+    navBarElem.getUrl();
   });
 
   it('should have a Navigation bar', function() {
-    expect(navBar.isDisplayed()).toBeTruthy();
+    expect(navBarElem.getNavBar().isDisplayed()).toBeTruthy();
   });
 
   it('should have three items', function() {
-    expect(navBar.all(by.tagName('li')).count()).toBe(3)
+    expect(navBarElem.getNavBar().all(by.tagName('li')).count()).toBe(3)
   });
 });
