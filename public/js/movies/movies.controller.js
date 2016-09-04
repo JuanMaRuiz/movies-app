ac891cb516380496e697d09e873aaddd4a241c85
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-09-05 00:12:06 +0200

    Added tests

---
+++ public/js/movies/movies.controller.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+  angular
+    .module('moviesModule')
+    .controller('MoviesController', MoviesController);
+
+    function MoviesController() {
+      var vm = this;
+    }
+})()
--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  // framework: 'jasmine',
+  framework: 'jasmine',
   seleniumServerJar: '../node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
   /* If specified browserstackUser and browserstackKey, the settings for seleniumServerJar, seleniumPort, eleniumArgs, sauceUser and sauceKey will be ignored */
   // seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -8,8 +8,9 @@ exports.config = {
     'browserName': 'chrome'
   },
   suites: {
-    login: './login/**/*Spec.js',
-    // search: ['tests/e2e/contact_search/**/*Spec.js',
-    //   'tests/e2e/venue_search/**/*Spec.js']
-  }
+    login: './login/**/*Spec.js'
+  },
+  plugins: [{
+    package: 'protractor-console',
+  }]
 }
